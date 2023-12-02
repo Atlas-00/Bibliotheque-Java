024c3e06d0271ecd67d6f0186757600525ee545c
Atlas-00 <121752652+Atlas-00@users.noreply.github.com>
2023-12-02 15:04:15 -0300

    Ajout du menu utilisateur et variable global des menbre et des livre

--- src/App.java
+++ src/App.java
@@ -1,6 +1,31 @@
+import fr.atlas.*;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Bibliotheque bibliotheque = new Bibliotheque();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bienvenue dans la bibliotheque de Atlas que voulez vous faire !");
+        System.out.println("--------------------------------\n");
+
+        System.out.println("1. Créer un compte :");
+        System.out.println("2. Se conecter :");
+        System.out.println("3. Emprunter un livre :");
+        System.out.println("4. Rendre un livre :");
+        System.out.println("5. Voire la iste des livre :");
+
+        System.out.print("\nEntrez  votre choix : ");
+        int choix = scanner.nextInt();
+
+        switch (choix) {
+            case 1:
+                bibliotheque.ajouterMembre();
+                break;
+
+            default:
+                System.out.println("Votre choix est invalide !!!");
+                break;
+        }
     }
 }
--- src/fr/atlas/Bibliotheque.java
+++ src/fr/atlas/Bibliotheque.java
@@ -1,9 +1,29 @@
 package fr.atlas;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Bibliotheque {
-  
-  public Bibliotheque() {
 
-  }
+    public Scanner scanner;
+    ArrayList<Membre> _membres;
+
+    public Bibliotheque() {
+        this.scanner = new Scanner(System.in);
+        this._membres = new ArrayList<Membre>();
+    }
+
+    public void ajouterMembre() {
+        System.out.print("\nEntrez votre nom d'utilisateur :");
+        String nomUtilisateur = scanner.nextLine();
+
+        System.out.print("Entrez votre email :");
+        String email = scanner.nextLine();
+
+        System.out.print("Entrez votre mot de passe :");
+        String password = scanner.nextLine();
+
+        _membres.add(new Membre(nomUtilisateur, email, password));
+        System.out.println("Votre compte à été créer !");
+    }
 }
-
