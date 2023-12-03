18e83e3c425c075486103ede3362356ffffbf922
Atlas-00 <121752652+Atlas-00@users.noreply.github.com>
2023-12-03 16:16:30 -0300

    Version stable de main

--- src/fr/atlas/Bibliotheque.java
+++ src/fr/atlas/Bibliotheque.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bibliotheque implements Empruntable, Affichable {
-    
     ArrayList<Membre> _membres;
     ArrayList<Livre> _livres;
     private Membre _membreActuel;
@@ -39,13 +38,6 @@ public class Bibliotheque implements Empruntable, Affichable {
         System.out.print("Entrez le nom de l'auteur : ");
         String auteur = scanner.next();
 
-        System.out.print("Entrez l'état du livre : ");
-        boolean etat = Boolean.parseBoolean(scanner.next());
-
-        System.out.print("Entrez l'année de publication : ");
-        int annee = scanner.nextInt();
-
-        _livres.add(new Livre(titre, auteur, etat, annee));
         System.out.print("Entrez l'année de publication : ");
         int annee = scanner.nextInt();
 
@@ -77,23 +69,6 @@ public class Bibliotheque implements Empruntable, Affichable {
         }
 
         boolean deconnect = false;
-
-<<<<<<< HEAD
-        System.out.println("\n1. Emprunter un livre :");
-        System.out.println("2. Rendre un livre :");
-        System.out.println("3. Voire mes livre enprunter :");
-
-        System.out.print("\nEntrez votre choix : ");
-        int choix = scanner.nextInt();
-
-        switch (choix) {
-            case 1:
-                emprunter();
-                break;
-
-            case 2:
-                retourner();
-                break;
 
         while (!deconnect) {
             System.out.println("\n--------------------------------");
@@ -136,11 +111,15 @@ public class Bibliotheque implements Empruntable, Affichable {
             }
         }
     }
-}
 
     @Override
     public void emprunter() {
         Scanner scanner = new Scanner(System.in);
+
+        if (_livres.isEmpty()) {
+            System.out.println("Aucun livre disponible!");
+            return;
+        }
 
         System.out.print("\nEntrez le nom du livre à emprunter : ");
         String nomLivre = scanner.nextLine();
@@ -150,8 +129,7 @@ public class Bibliotheque implements Empruntable, Affichable {
         for (Livre livre : _livres) {
             if (livre.getNomLivre().contains(nomLivre) && livre.isEtat()) {
                 livre.setEtat(false);
-                System.out.println("Le livre '" + livre.getNomLivre() + "' a été emprunté par "
-                        + _membreActuel.getmNomUtilisateur());
+                _membreActuel.emprunter(livre);
                 livreTrouve = true;
                 break; // Sort de la boucle une fois que le livre a été emprunté
             }
@@ -164,7 +142,44 @@ public class Bibliotheque implements Empruntable, Affichable {
 
     @Override
     public void retourner() {
-        throw new UnsupportedOperationException("Méthode 'retourner' non implémentée");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("\nEntrez le nom du livre à retourner : ");
+        String nomLivre = scanner.nextLine();
+
+        boolean livreTrouve = false;
+
+        for (Livre livre : _livres) {
+            if (livre.getNomLivre().contains(nomLivre) && !livre.isEtat()) {
+                livre.setEtat(true);
+                _membreActuel.retourner(livre);
+                livreTrouve = true;
+                break; // Sort de la boucle une fois que le livre a été retourné
+            }
+        }
+
+        if (!livreTrouve) {
+            System.out.println("Ce livre n'existe pas ou n'est pas disponible!");
+        }
+    }
+
+    @Override
+    public void afficher() {
+        if (_livres.isEmpty()) {
+            System.out.println("Aucun livre disponible!");
+            return;
+        }
+
+        System.out.println("\nLes livres disponibles : ");
+        int numberLivre = 0;
+
+        for (Livre livre : _livres) {
+            numberLivre++;
+            System.out.println("\nLivre n°" + numberLivre + ". " + livre);
+        }
+    }
+
+    public void livresEmpruntes() { 
+        _membreActuel.livresEmpruntes();
     }
 }
-
